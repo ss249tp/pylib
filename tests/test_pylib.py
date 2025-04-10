d3d9cb3e3a5d1cd1d2e3d0e0eebafdebf845ca4a
ss249tp <ss249.tp@gmail.com>
2025-04-10 13:32:20 +0800

    test: Add test for `false()`

--- tests/test_pylib.py
+++ tests/test_pylib.py
@@ -5,3 +5,9 @@ def test_true():
     assert pylib.true()
     assert pylib.true(0)
     assert pylib.true("123")
+
+
+def test_false():
+    assert not pylib.false()
+    assert not pylib.false(0)
+    assert not pylib.false("123")
